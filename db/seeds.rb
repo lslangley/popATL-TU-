9869df8f1a73a884a075db68d9462ebdd2564a06
Leslie <lcaceda11@gmail.com>
2016-04-29 04:52:51 -0400

    admin view not properly routed. one admin added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(first_name: 'Leslie', Last_name: 'Caceda', email: 'lcaceda11@gmail.com', password: 'Poppins11!', role: 'admin')
